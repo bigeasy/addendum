01eb3e042fe69fb7c495d6cee962e9589445acc0
Alan Gutierrez <alan@prettyrobots.com>
2013-04-07 12:04:45 -0400

    Export `Addendum` from `index.js`.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var __slice = [];
 
-module.exports = function () {
-  var a = 1;
-  var b = 1;
-  return a + b;
-};
+module.exports = Addendum;
 
 function Addendum () {
 }
@@ -20,9 +16,6 @@ function Table () {
   this._operations = [];
 }
 
-Table.prototype.varying = function (name, length) {
-}
-
 function capture (object, method) {
   var $ = /^(\w+)\(([^)]+)\)$/.exec(method);
   var method = $[1], parameters = $[2].split(/\s*,\s*/);
@@ -35,32 +28,14 @@ function capture (object, method) {
   }
 }
 
+capture(Table.prototype, 'varying(name, length)');
 capture(Table.prototype, 'autoid(name)');
 
 Amendment.prototype.createTable = function (name) {
-  this_operations.push({ type: 'useSchema', name: name });
+  this._operations.push({ type: 'useSchema', name: name });
 }
 
 Addendum.amend = function (builder) {
   var amendment = new Amendment(this);
   builder(amendment);
-}
-
-exports.addendum = function (addendum) {
-  addendum.amend(function (amendment) {
-    addendum.createSchema("omnicorp");
-    addendum.useSchema("omnicorp");
-    addendum.createTable("employees", function (table) {
-      table.autoid("id");
-      table.varying("first_name", 64);
-      table.varying("last_name", 64);
-      table.timestamp("updated");
-      table.timestamp("created");
-    });
-  });
-  addendum.amend(function (mutator) {
-    mutator.insert("employees", { first_name: "Alan", last_name: "Gutierrez" });
-    mutator.insert("employees", { first_name: "John", last_name: "Smith" });
-    mutator.insert("employees", { first_name: "Priya", last_name: "Patel" });
-  });
 }
