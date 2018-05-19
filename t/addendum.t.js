a455c13a6bcae7e2f8e419f345c4d77306a85f61
Alan Gutierrez <alan@prettyrobots.com>
2018-05-19 11:26:33 -0500

    Test construction.

--- t/addendum.t.js
+++ t/addendum.t.js
@@ -2,5 +2,5 @@ require('proof')(1, require('cadence')(prove))
 
 function prove (async, okay) {
     var Addendum = require('../addendum')
-    okay(Addendum, 'required')
+    okay(new Addendum, 'construct')
 }
