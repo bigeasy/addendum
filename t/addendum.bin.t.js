75a20401d218d100af92b987aa20bd7e4655606a
Alan Gutierrez <alan@prettyrobots.com>
2018-06-27 15:33:20 -0500

    Create dummy Compassion to test main function.

--- t/addendum.bin.t.js
+++ t/addendum.bin.t.js
@@ -3,15 +3,37 @@ require('proof')(2, require('cadence')(prove))
 function prove (async, okay) {
     var bin = require('../addendum.bin'), program
 
+    var http = require('http')
+
+    var cadence = require('cadence')
+    var destroyer = require('server-destroy')
+    var delta = require('delta')
+
+    var Reactor = require('reactor')
+
     var Destructible = require('destructible')
     var destructible = new Destructible('t/addendum.bin')
 
     var UserAgent = require('vizsla')
     var ua = new UserAgent
 
+    var compassion = new Reactor({
+        register: cadence(function (async) {
+            return 200
+        })
+    }, function (dispatcher) {
+        dispatcher.dispatch('/register', 'register')
+    })
+
     async([function () {
         destructible.destroy()
     }], function () {
+        var server = http.createServer(compassion.middleware)
+        destroyer(server)
+        destructible.destruct.wait(server, 'destroy')
+        delta(destructible.monitor('compassion')).ee(server).on('end')
+        server.listen(8386, async())
+    }, function () {
         program = bin({
             public: 8080,
             local: 8086,
