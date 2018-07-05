07c6c695fa4f9603dacde53c8a15044e28b65548
Alan Gutierrez <alan@prettyrobots.com>
2018-07-04 23:42:07 -0500

    Get logger running in container.

--- minikube/config/addendum.k8s.js
+++ minikube/config/addendum.k8s.js
@@ -1,28 +1,39 @@
 console.log(JSON.stringify({
     kind: 'Deployment',
-    apiVersion: 'extension/v1beta1',
+    apiVersion: 'extensions/v1beta1',
     metadata: {
         name: 'addendum',
         namespace: 'addendum',
         labels: {
             name: 'addendum',
             environment: 'minikube'
+        }
+    },
+    spec: {
+        replicas: 1,
+        selector: {
+            matchLabels: {
+                name: 'addendum',
+                environment: 'minikube'
+            }
         },
-        spec: {
-            replicas: 1,
-            selector: {
-                matchLabels: {
+        template: {
+            metadata: {
+                labels: {
                     name: 'addendum',
                     environment: 'minikube'
                 }
             },
-            template: {
-                metadata: {
-                    labels: {
-                        name: 'addendum',
-                        environment: 'minikube'
-                    }
-                }
+            spec: {
+                restartPolicy: 'Always',
+                terminationGracePeriodSeconds: 5,
+                dnsPolicy: 'ClusterFirst',
+                containers: [{
+                    name: 'logger',
+                    image: 'bigeasy/addendum:kube',
+                    imagePullPolicy: 'Never',
+                    command: [ '/home/bigeasy/addendum/bin/logger' ]
+                }]
             }
         }
     }
